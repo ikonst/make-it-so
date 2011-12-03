1334d22a3079f2650378507e690ed77f9cd44b84
Richard Shepherd <richard.s.shepherd@gmail.com>
2011-12-03 20:02:09 +0000

    Test for replacing include paths.

---
+++ Tests/TestProjects/VS2008/ReplaceIncludePaths/App/main.cpp
@@ -0,0 +1,8 @@
+#include <ShowText.h>
+
+
+int main(int argc, char** argv)
+{
+	ShowText::showText();
+	return 0;
+}
---
+++ Tests/TestProjects/VS2008/ReplaceIncludePaths/ShowText_Goodbye/ShowText.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <stdio.h>
+
+class ShowText
+{
+public:
+	static void showText()
+	{
+		printf("Goodbye cruel world!");
+	}
+};
