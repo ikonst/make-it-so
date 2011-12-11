7e79a00429a699f34604daa7909eaa4b35db67bc
Richard Shepherd <richard.s.shepherd@gmail.com>
2011-12-11 18:54:35 +0000

    Some support for paths and configurations with spaces.

---
+++ Tests/TestProjects/VS2008/Paths With Spaces/Hello Library/Hello.cpp
@@ -0,0 +1,6 @@
+#include "Hello.h"
+
+std::string Hello::getHello()
+{
+	return "Hello";
+}
---
+++ Tests/TestProjects/VS2008/Paths With Spaces/Hello Library/Hello.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <string>
+
+class Hello
+{
+public:
+	static std::string getHello();
+};
