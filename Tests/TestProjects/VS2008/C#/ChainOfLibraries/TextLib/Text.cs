ec75aeaec840a36e4751c6ceb5b45a5ea89903d0
Richard Shepherd <richard.s.shepherd@gmail.com>
2011-12-28 19:00:03 +0000

    C# tests.

---
+++ Tests/TestProjects/VS2008/C#/ChainOfLibraries/TextLib/Text.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HelloLib;
+using WorldLib;
+
+namespace TextLib
+{
+    public class Text
+    {
+        public static string getText()
+        {
+            return Hello.getText() + ", " + World.getText() + "!";
+        }
+    }
+}
