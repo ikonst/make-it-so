42fdd3bca22bc44fe280217ee4b3e541715d4a1e
Richard Shepherd <richard.s.shepherd@gmail.com>
2012-01-01 13:40:40 +0000

    Working on support for C# projects.

---
+++ Tests/TestProjects/VS2008/C#/WinformsAppWithCustomControl/App/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.userControl11 = new ReverseTextBox.UserControl1();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // userControl11
+            // 
+            this.userControl11.Location = new System.Drawing.Point(12, 31);
+            this.userControl11.Name = "userControl11";
+            this.userControl11.Size = new System.Drawing.Size(183, 30);
+            this.userControl11.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.userControl11);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ReverseTextBox.UserControl1 userControl11;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Tests/TestProjects/VS2008/C#/WinformsAppWithCustomControl/App/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            userControl11.setText("!dlroW ,olleH");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string text = userControl11.getText();
+            File.WriteAllText("testOutput.txt", text);
+            Close();
+        }
+    }
+}
