edec83a069f7bfe2e6c4184074dff1b39e0b988a
Richard Shepherd <richard.s.shepherd@gmail.com>
2012-01-01 13:10:43 +0000

    Working on support for C# projects.

---
+++ Tests/TestProjects/VS2008/C#/ChainOfLibrariesWithExternal/App/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TextLib;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(Text.getText());
+        }
+    }
+}
