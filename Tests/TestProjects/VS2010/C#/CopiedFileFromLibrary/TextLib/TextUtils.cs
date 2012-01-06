b9ca711141f40ea15c5166c0a01470631587c09c
Richard Shepherd <richard.s.shepherd@gmail.com>
2012-01-06 22:26:36 +0000

    Adding VS2010 support and tests.

---
+++ Tests/TestProjects/VS2010/C#/CopiedFileFromLibrary/TextLib/TextUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace TextLib
+{
+    public class TextUtils
+    {
+        public static string getText()
+        {
+            try
+            {
+                return File.ReadAllText("Hello.txt");
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
