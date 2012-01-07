e52dfe5e72dfbf3552b78516c60b1913e019f596
Richard Shepherd <richard.s.shepherd@gmail.com>
2012-01-07 20:08:41 +0000

    Adding VS2010 support and tests.

---
+++ Tests/TestProjects/VS2010/C#/AppWithLibrary/TextLib/TextUtils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TextLib
+{
+    public class TextUtils
+    {
+        public static string getText()
+        {
+            return "Hello, World!";
+        }
+    }
+}
