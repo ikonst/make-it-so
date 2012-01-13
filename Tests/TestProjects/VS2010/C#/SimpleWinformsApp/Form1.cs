e4841431d11f969c611fcde9c38ececd1036c600
Richard Shepherd <richard.s.shepherd@gmail.com>
2012-01-13 20:52:41 +0000

    Adding VS2010 support and tests.

---
+++ Tests/TestProjects/VS2010/C#/SimpleWinformsApp/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SimpleWinformsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            File.WriteAllText("testOutput.txt", "Hello, World!");
+            Close();
+        }
+    }
+}
